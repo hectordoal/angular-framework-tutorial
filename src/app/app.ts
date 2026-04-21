d3283f503a70dd56e99a591d7d1641bc9460aba8
hectordoal <hectordoal23@gmail.com>
2026-04-21 21:11:04 +0200

    Comienzo de tutorial angular, con limpieza de código, imágenes, estilos de botones (CSS), etc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Esto permite usar @if y @for
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // Importamos las herramientas básicas
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('tutorial_angular');
+export class AppComponent {
+  // Esta variable guardará en qué paso del tutorial estamos
+  pasoActual: number = 0; 
+
+  // Esta función cambia el paso cuando hacemos clic
+  cambiarPaso(nuevoPaso: number) {
+    this.pasoActual = nuevoPaso;
+  }
 }
